c1ea5c32a8eb8c9e0c07328d028ceef6bdb206bd
Marney Pratt <mcpratt@smith.edu>
2021-11-22 14:01:54 -0500

    changed code to keep zeros when certain FFgs are not present and also to add variables of interest back to the final data frame

--- script_templates/data_wrangling/FFG.density.R
+++ script_templates/data_wrangling/FFG.density.R
@@ -13,12 +13,32 @@
   
   
   # Sum each FFG density for each sampleID 
-  #replace blank with grouping variables as needed such as season, location, year, etc
-  group_by(sampleID, FFG, ___) %>% 
+  group_by(sampleID, FFG) %>% 
   dplyr::summarise (ffgDens = sum(invDens, na.rm = TRUE))
 
 
   #adds back the zeros for FFGs that were not present in a sample
   #repeat all the grouping variables as above
   ungroup %>%
-    complete(sampleID, FFG, ___, fill = list(ffgDens = 0)) 
+    complete(sampleID, FFG, fill = list(ffgDens = 0)) 
+  
+  
+  #filter for the organisms without FFGs assigned
+  macro.ffg.na <- macro.ffg %>% 
+    filter(is.na(FFG) & ffgDens > 0)
+  
+  # select other variables you want present in your final dataset
+  variables <- ____ %>% # put original data in this blank (macro or macro.env)
+    
+    #replace blank with variables from the original dataset that you want present 
+    select(sampleID, date, location, year, season, ___) %>% 
+    distinct()
+  
+  
+  #add back the organisms without FFGs assigned
+  macro.ffg <- macro.ffg %>% 
+    filter(!is.na(FFG)) %>% 
+    bind_rows(macro.ffg.na) %>% 
+    
+    #add variables of interest to final dataset
+    left_join(., variables)
